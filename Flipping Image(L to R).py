fc45aeeb53612c837dda31f26d5f4caa4851ad05
Anu Bagre <112414078+anubagre@users.noreply.github.com>
2023-08-11 16:00:56 +0530

    Flipping Image(L to R).py

--- Flipping Image(L to R).py
+++ Flipping Image(L to R).py
@@ -3,10 +3,8 @@
 Created on Fri Aug 11 11:38:58 2023
 @author: HP
 """
-
 #Flippin Image(L to R)
 from PIL import Image
 img=Image.open('corrected.jpg')
 t_img=img.transpose(Image.FLIP_LEFT_RIGHT)
-#t_img=img.Tranpose.FLIP_LEFT_RIGHT
 t_img.save('new.jpg')
