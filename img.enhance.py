5c95d9a118941897e7bb0c8a73e7399977c91b7c
Anu Bagre <112414078+anubagre@users.noreply.github.com>
2023-08-11 15:58:29 +0530

    img.enhance.py

--- img.enhance.py
+++ img.enhance.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Feb 16 22:48:00 2023
-
 @author: HP
 """
-
-'''from PIL import Image
-img=Image.open(r"C:\\Users\HP\OneDrive\Pictures\canva\649-07118141en_Masterfile.jpg")
-img2=img.Transpose.FLIP_LEFT_RIGHT
-img2.save("corrected.jpg")'''
-
 import opencv as cv2
 img=cv2.imread(r"C:\\Users\HP\OneDrive\Pictures\canva\649-07118141en_Masterfile.jpg")
 #Preparartion for CLAHE
@@ -18,5 +11,3 @@ clahe=cv2.createCLAHE()
 gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 enh_i=clahe.apply(gray_img)
 cv2.imwrite('enhanced.jpg',enh_i)
-
-
